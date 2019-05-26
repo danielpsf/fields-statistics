d46e7ead3941bf3555b9f7470f562e500b94bb6c
Daniel Fernandes <3307367+danielpsf@users.noreply.github.com>
2019-05-26 11:52:03 -0300

    Tidy up project organization - part 3

--- src/main/java/com/danielpsf/labs/field/statistic/report/repository/StatisticRepository.java
+++ src/main/java/com/danielpsf/labs/field/statistic/report/repository/StatisticRepository.java
@@ -2,13 +2,13 @@ package com.danielpsf.labs.field.statistic.report.repository;
 
 import com.danielpsf.labs.field.statistic.metric.domain.Metric;
 import com.danielpsf.labs.field.statistic.report.domain.StatisticData;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 
-public interface StatisticRepository extends JpaRepository<Metric, Long> {
+public interface StatisticRepository extends Repository<Metric, Long> {
 
     @Query(value = "SELECT " +
             "NEW com.danielpsf.labs.field.statistic.report.domain.StatisticData(" +
